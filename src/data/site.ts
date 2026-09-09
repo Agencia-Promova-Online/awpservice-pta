1adc4791232fa9ab684f7c1a0d1944ee823ba52f
Pr0m0v4 <agencia.promovaonline@gmail.com>
2026-09-09 18:04:27 -0300

    link embed personalizado

--- src/data/site.ts
+++ src/data/site.ts
@@ -18,7 +18,7 @@ export const siteConfig = {
     country: 'Brasil',
   },
   mapsEmbed:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31862489.56503199!2d-51.3168626!3d-14.40952615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88409110db442eaf%3A0xd5148e9a35ff3b21!2sAWP%20Service%20PTA!5e1!3m2!1spt-BR!2sbr!4v1788987381684!5m2!1spt-BR!2sbr',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3476.316138337895!2d-51.2017903!3d-29.1519182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88409110db442eaf%3A0xd5148e9a35ff3b21!2sAWP%20Service%20PTA!5e1!3m2!1spt-BR!2sbr!4v1788987381684!5m2!1spt-BR!2sbr',
   mapsLink: 'https://maps.app.goo.gl/FzBik8gJWqCLCTVJ7',
   hours: [
     { day: 'Segunda a Sexta', time: '07:30 — 18:00' },
